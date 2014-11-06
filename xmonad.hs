886d64a69db5cd2216137d7b45fc7b52e7519a10
Simon Swain <simon@simonswain.com>
2014-11-07 10:10:28 +1100

    Ubuntu 14.04 fix

--- xmonad.hs
+++ xmonad.hs
@@ -17,7 +17,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
  
     -- launch dmenu
-    , ((modm,               xK_d     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+    , ((modm,               xK_d     ), spawn "exe=`dmenu_path | dmenu_run` && eval \"exec $exe\"")
  
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
